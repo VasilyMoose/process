9951c47720447ec90c1a3da564130526ceb15c25
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-19 00:36:55 +0300

    Create lsof.sh

---
+++ lsof.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+find /proc/[0-9]*/fd -type l -exec ls -l {} \; > test
+
+while IFS= read -r line
+do
+  l=$(echo $line)
+  U=`echo $l | awk '{print $3}'`
+  P=$(echo $l | awk '{print $11}')
+  var_pid=$(echo $l | awk '{print $9}' | grep -o "/[0-9]*/" | grep -o [0-9]* )
+  var_t=$(echo $l | awk '{print $8}')
+  var_d=$(echo $l | awk '{print $7}')
+  var_m=$(echo $l | awk '{print $6}')
+  echo $U '|' $var_m '|' $var_d '|'  $var_t '|' $var_pid '|' $P   
+done < test
+rm -rf test
